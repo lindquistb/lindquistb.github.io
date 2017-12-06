417c0862579b967d302a421f411afb8c82747f1b
lindquistb <34040620+lindquistb@users.noreply.github.com>
2017-12-06 02:49:02 -0700

    Second Change

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
-var img;
-var song;
+var joker;
+var jokersound;
 var amp;
-var fft;
-var band;
-var g;
+
+
 function preload(){
-  img = loadImage("images/starry.png")
-  song = loadSound("songs/edub.mp3");
+  joker = loadImage("images/joker.png")
+  jokersound = loadSound("songs/joker2.mp3");
   
 }
 
@@ -14,16 +13,19 @@ function setup() {
   createCanvas(screen.width, screen.height);
   background(0);
   amp = new p5.Amplitude();
-  amp.setInput(song);
-  fft = new p5.FFT(0,16);
-  band = (width / 16);
-  song.loop();
-  g= width;
+  amp.setInput(jokersound);
+  
+  //song.loop();
+  //jokersound.loop();
+ 
   scene1= 0;
 }
-function keyPressed(){
-    if(keyCode == LEFT_ARROW){
+
+
+function keyTyped(){
+    if(key == "1"){
       scene1 = 1;
+      jokersound.play();
     } else{
       scene1 = 0;
   }
@@ -36,51 +38,71 @@ function draw() {
   //background(0);
   var rms = amp.getLevel();
   var qrs = rms*100;
-  var srs = 0;
+  //var srs = 0;
+  //var crs = 0;
   var speed = 100;
   if(scene1 == 1){
-    translate(width/12, height/12);
-  if(qrs < 2){
+    
+  if(qrs < 0.2){
     crs = 0;
     srs = 0;
-  
   }
+  
+  if(qrs > 0.2){
+    crs = 1;
+    srs = 0;
+  }
+  
+   if(qrs > 1){
+    crs = 0;
+    srs = 1;
+  }
+  
+  if(qrs > 2){
+    crs = 2;
+    srs = 0;
+  }
+  
   if(qrs > 4){
     crs = 4;
     srs = 0;
-    //speed = 2;
-  
-  }
+   }
+   
   if (qrs > 8){
     crs = 4;
     srs = 4;
-   // speed = 10;
+   }
    
-  }
   if (qrs > 12){
-    crs = 20;
-    srs = 20;
+    crs = 8;
+    srs = 8;
   }
 
   if (qrs > 15){
+    crs = 16;
+    srs = 16;
+  }
+  
+   if (qrs > 20){
     crs = 30;
     srs = 30;
-    //speed = 50;
   }
+  
   if (qrs > 40){
     crs = 30;
     srs = 30;
-   // speed = 100;
- 
-  }
+   }
  
   console.log(qrs);
+  
+  
+  translate(200,0);
   for(var i = 0; i < speed ; i++){
-   
+  
     var x = random(width);
     var y = random(height);
-    var c = img.get(x,y);
-    
+    var c = joker.get(x,y);
+    //crs = 0;
     fill(c);
     noStroke();
     
