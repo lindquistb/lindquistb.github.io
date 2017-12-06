8c7ad925abcac612623e436d98a4e1187ff3f8a7
lindquistb <34040620+lindquistb@users.noreply.github.com>
2017-12-06 05:56:59 -0700

    trying to figure out text

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var amp;
 
 
 function preload(){
+  
   joker = loadImage("images/joker.png");
   skyrim = loadImage("images/skyrim.png");
   jokersound = loadSound("songs/joker2.mp3");
@@ -14,6 +15,7 @@ function preload(){
 }
 
 function setup() {
+  
   createCanvas(screen.width, screen.height);
   background(0);
   amp = new p5.Amplitude();
@@ -26,11 +28,24 @@ function setup() {
 }
 
 function keyTyped(){
+  
+  if(key == "0"){
+      scene = 0;
+      background(0);
+      if(skyrimsound.isPlaying()){
+      skyrimsound.stop();}
+      if(jokersound.isPlaying()){
+      jokersound.stop();}
+      resizeCanvas(windowWidth,windowHeight);
+      sentence = "Oh Hello There asd;jasdlgjasdl;gjkasldjkgasdl;kgjasdlgjk;ag"
+    fill(255);
+    textAlign(CENTER);
+    textSize(10);
+    text(sentence,windowWidth/2,windowHeight/2,80,100);
+  }
+  
     if(key == "1"){
-      //clear();
-      //background();
       scene = 1;
-      //clear();
       background(0);
       skyrimsound.stop();
       if(jokersound.isPlaying()){
@@ -38,15 +53,12 @@ function keyTyped(){
       else{
         jokersound.play();
       }
-    
-      
     }
     
     if(key == "2"){
-      //clear();
-     // background(0);
+ 
+      
       scene = 2;
-      //clear();
       background(0);
       jokersound.stop();
       
@@ -55,13 +67,9 @@ function keyTyped(){
       else{
         skyrimsound.play();
       }
-      //amp.setInput(skyrimsound);
       noStroke();
-      
     }
-     //else{
-      //scene = 0;
-   //}
+    
 }
 
 function draw() {
@@ -71,6 +79,16 @@ function draw() {
   var rms = amp.getLevel();
   var qrs = rms*100;
   var speed = 100;
+  
+  if(scene == 0){
+    background(0);
+    sentence = "Oh Hello There asd;jasdlgjasdl;gjkasldjkgasdl;kgjasdlgjk;ag"
+    fill(255);
+    textAlign(CENTER);
+    textSize(10);
+    text(sentence,windowWidth/2,windowHeight/2,80,100);
+    //resizeCanvas(windowWidth, windowHeight);
+  }
   
   if(scene == 1){
     
@@ -142,13 +160,8 @@ function draw() {
   
   if(scene == 2){
     
-    if(qrs < 0.2){
+  if(qrs < 1){
     crs = 0;
-    srs = 0;
-  }
-  
-  if(qrs > 0.2){
-    crs = 1;
     srs = 0;
   }
   
@@ -169,25 +182,35 @@ function draw() {
    
   if (qrs > 8){
     crs = 4;
-    srs = 4;
+    srs = 0;
    }
    
-  if (qrs > 12){
-    crs = 8;
-    srs = 8;
+  if (qrs > 10){
+    crs = 0;
+    srs = 4;
   }
 
+  if (qrs > 12){
+    crs = 4;
+    srs = 4;
+  }
+  
   if (qrs > 15){
-    crs = 16;
-    srs = 16;
+    crs = 15;
+    srs = 15;
   }
   
    if (qrs > 20){
-    crs = 30;
-    srs = 30;
+    crs = 20;
+    srs = 20;
   }
   
-  if (qrs > 40){
+   if (qrs > 25){
+    crs = 30;
+    srs = 0;
+  }
+  
+  if (qrs > 30){
     crs = 30;
     srs = 30;
    }
@@ -208,10 +231,5 @@ function draw() {
     ellipse(x,y,crs,crs);
      }
   }
-
-//else{
-  //fill(0);
-  //rect(0,0,screen.width,screen.height);
-//}
 
 }
